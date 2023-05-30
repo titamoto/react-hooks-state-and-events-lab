7526654b6fba7ccdf48dd75c20b80ef3b5a6cacb
Aleksandr Savchenko <titamoto@gmail.com>
2023-05-30 11:47:45 -0700

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addedToCart, setAddedToCart] = useState(false);
+  const inCart = addedToCart ? "in-cart" : "";
+  const buttonText = addedToCart ? "Remove From Cart" : "Add to Cart";
+  const buttonColor = addedToCart ? "remove" : "add";
+
+  function handleClick() {
+    setAddedToCart((addedToCart) => !addedToCart);
+  }
+
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonColor} onClick={handleClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
